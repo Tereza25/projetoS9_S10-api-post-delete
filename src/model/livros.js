039fa379502c123ee7b059e05174154ef48e4338
tereza25 <rh.cris.ead@gmail.com>
2020-10-18 01:47:10 -0300

    modificações

--- src/model/livros.js
+++ src/model/livros.js
@@ -1,61 +1,61 @@
 [
     {   
         "id": 1,
-        "Titulo": "Olhos mais azuis",
+        "Nome": "Olhos mais azuis",
         "Autoria": "Toni Morrison",
         "Editora": "Companhia das letras"
     },
     {
         "id": 2,
-        "Titulo": "A cor purpura",
+        "Nome": "A cor purpura",
         "Autoria": "Alice Walker",
         "Editora": "Marco Zero"
     },
     {
         "id": 3,
-        "Titulo": "Olhos D'agua",
+        "Nome": "Olhos D'agua",
         "Autoria": "Conceicao Evaristo",
         "Editora": "Pallas"
     },
     {
         "id": 4,
-        "Titulo": "Hibisco Roxo",
+        "Nome": "Hibisco Roxo",
         "Autoria": "Chimamanda Ngozi",
         "Editora":"Companhia das letras"
     },
     {
         "id": 5,
-        "Titulo": "Mulheres, raça e classe",
+        "Nome": "Mulheres, raça e classe",
         "Autoria": "Angelas Davis",
         "Editora": "Boitempo"
     },
     {
         "id": 6,
-        "Titulo": "Lugar de fala",
+        "Nome": "Lugar de fala",
         "Autoria": "Djamila Ribeiro",
         "Editora": "Polen Livros",
     },
     {
         "id": 7,
-        "Titulo": "Racismo Estrutural",
+        "Nome": "Racismo Estrutural",
         "Autoria": "Silvio de Almeida",
         "Editora": "Polen Livros"
     },
     {
         "id": 8,
-        "Titulo": "Manual antiracista",
+        "Nome": "Manual antiracista",
         "Autoria": "Djamila Ribeiro",
         "Editora": "Compainha das letras"
     },
     {
         "id": 9,
-        "Titulo": "O genocidio do negro brasileiro",
+        "Nome": "O genocidio do negro brasileiro",
         "Autoria": "Abdias Nascimento",
         "Editora": "Perspectiva"
     },
     {
         "id": 10,
-        "Titulo": "Um defeito de cor",
+        "Nome": "Um defeito de cor",
         "Autoria": "Ana Maria Gonçalves",
         "Editora": "Record"
     }
