a5851ba6ab9c437f6de824310391e81df19f287c
tereza25 <rh.cris.ead@gmail.com>
2020-10-18 00:56:40 -0300

    criação das pastas e instalação das dependências

---
+++ src/model/livros.js
@@ -0,0 +1,63 @@
+[
+    {   
+        "id": 1,
+        "Titulo": "Olhos mais azuis",
+        "Autoria": "Toni Morrison",
+        "Editora": "Companhia das letras"
+    },
+    {
+        "id": 2,
+        "Titulo": "A cor purpura",
+        "Autoria": "Alice Walker",
+        "Editora": "Marco Zero"
+    },
+    {
+        "id": 3,
+        "Titulo": "Olhos D'agua",
+        "Autoria": "Conceicao Evaristo",
+        "Editora": "Pallas"
+    },
+    {
+        "id": 4,
+        "Titulo": "Hibisco Roxo",
+        "Autoria": "Chimamanda Ngozi",
+        "Editora":"Companhia das letras"
+    },
+    {
+        "id": 5,
+        "Titulo": "Mulheres, raça e classe",
+        "Autoria": "Angelas Davis",
+        "Editora": "Boitempo"
+    },
+    {
+        "id": 6,
+        "Titulo": "Lugar de fala",
+        "Autoria": "Djamila Ribeiro",
+        "Editora": "Polen Livros",
+    },
+    {
+        "id": 7,
+        "Titulo": "Racismo Estrutural",
+        "Autoria": "Silvio de Almeida",
+        "Editora": "Polen Livros"
+    },
+    {
+        "id": 8,
+        "Titulo": "Manual antiracista",
+        "Autoria": "Djamila Ribeiro",
+        "Editora": "Compainha das letras"
+    },
+    {
+        "id": 9,
+        "Titulo": "O genocidio do negro brasileiro",
+        "Autoria": "Abdias Nascimento",
+        "Editora": "Perspectiva"
+    },
+    {
+        "id": 10,
+        "Titulo": "Um defeito de cor",
+        "Autoria": "Ana Maria Gonçalves",
+        "Editora": "Record"
+    }
+
+]
